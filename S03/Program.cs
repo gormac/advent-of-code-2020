7a17a8e94304d18b8a721c3fc189dc3c5dc74035
Marco Glasbergen <Marco.Glasbergen@shell.com>
2020-12-03 18:14:52 +0100

    Day 3 Part 1

---
+++ S03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace S03
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            var position = 0;
+            var treeEncounters = 0;
+            var treeLines = GetTreeLines();
+            foreach (string treeLine in treeLines)
+            {
+                if (position > treeLine.Length - 1)
+                {
+                    position -= treeLine.Length;
+                }
+
+                if (treeLine[position] == '#')
+                {
+                    treeEncounters++;
+                }
+
+                position += 3;
+            }
+
+            Console.WriteLine($"Number of trees encountered: {treeEncounters}");
+        }
+
+        private static IEnumerable<string> GetTreeLines()
+        {
+            var lines = File.ReadAllLines("trees.txt");
+            return lines.ToList();
+        }
+    }
+}
