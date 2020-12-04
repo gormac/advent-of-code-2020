da5f7e7a9668fc2ddf2a1afa138e58b6b7b4e2cf
Marco Glasbergen <marco.glasbergen@gmail.com>
2020-12-05 00:40:13 +0100

    Day 4 Part 1

---
+++ S04/Passport.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace S04
+{
+    internal class Passport
+    {
+        // byr (Birth Year)
+        // cid (Country ID)
+        // ecl (Eye Color)
+        // eyr (Expiration Year)
+        // hcl (Hair Color)
+        // hgt (Height)
+        // iyr (Issue Year)
+        // pid (Passport ID)
+
+        private readonly List<string> _keys = new List<string>();
+        private readonly List<string> _mandatoryKeys = new List<string> { "byr", "ecl", "eyr", "hcl", "hgt", "iyr", "pid" };
+        private readonly Dictionary<string, string> _passportInfo = new Dictionary<string, string>();
+
+        public Passport(string passportLine)
+        {
+            ParsePassportInfo(passportLine);
+            _keys = new List<string>(_passportInfo.Keys);
+        }
+
+        private void ParsePassportInfo(string passportLine)
+        {
+            foreach (var keyvalue in passportLine.Split(' ').Where(keyvalue => !string.IsNullOrWhiteSpace(keyvalue)))
+            {
+                _passportInfo.Add(keyvalue.Split(':')[0], keyvalue.Split(':')[1]);
+            }
+        }
+
+        public bool IsValid => _mandatoryKeys.TrueForAll(key => _keys.Contains(key));
+    }
+}
---
+++ S04/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace S04
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Check those passports!");
+
+            var passports = GetPassports();
+            var validPassportsCount = passports.Where(passport => passport.IsValid).Count();
+
+            Console.WriteLine($"# of valid passports: {validPassportsCount}");
+        }
+
+        private static IEnumerable<Passport> GetPassports()
+        {
+            return (GetPassportLines().Select(passportLine => new Passport(passportLine))).ToList();
+        }
+
+        private static IEnumerable<string> GetPassportLines()
+        {
+            var lines = new List<string>();
+
+            using (StreamReader sr = new StreamReader("passports.txt"))
+            {
+                string line;
+                string passport = string.Empty;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        // Next passport
+                        lines.Add(passport);
+                        passport = string.Empty;
+                    }
+                    else
+                    {
+                        // Next passport line
+                        passport += line + " ";
+                    }
+                }
+
+                if (!string.IsNullOrWhiteSpace(passport))
+                {
+                    // Add the last passport
+                    lines.Add(passport);
+                }
+            }
+
+            return lines;
+        }
+    }
+}
