20b8fd4bedc5be27354a70d4d4a355aa9c4e14c7
Marco Glasbergen <Marco.Glasbergen@shell.com>
2020-12-08 10:21:23 +0100

    Day 5 Part 1

---
+++ S05/BoardingPass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace S05
+{
+    internal class BoardingPass
+    {
+        public BoardingPass(string seatCode)
+        {
+            string convertedCode = seatCode.Replace('F', '0').Replace('B', '1').Replace('L', '0').Replace('R', '1');
+            SeatId = Convert.ToInt32(convertedCode, 2);
+        }
+
+        internal int SeatId { get; }
+    }
+}
---
+++ S05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace S05
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Let's get that boarding pass!");
+
+            var boardingPasses = GetBoardingPasses();
+            var highestSeatId = boardingPasses.OrderByDescending(p => p.SeatId).First().SeatId;
+
+            Console.WriteLine($"Highest seat ID: {highestSeatId}");
+        }
+
+        private static IEnumerable<BoardingPass> GetBoardingPasses()
+        {
+            return GetSeatCodes().Select(seatCode => new BoardingPass(seatCode));
+        }
+
+        private static IEnumerable<string> GetSeatCodes()
+        {
+            return File.ReadAllLines("BoardingPasses.txt");
+        }
+    }
+}
