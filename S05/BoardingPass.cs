1c6f43de4017f85c8b85b201f7e08d1b750459cf
Marco Glasbergen <Marco.Glasbergen@shell.com>
2020-12-08 11:13:49 +0100

    Day 5 Part 2

--- S05/BoardingPass.cs
+++ S05/BoardingPass.cs
@@ -4,12 +4,16 @@ namespace S05
 {
     internal class BoardingPass
     {
-        public BoardingPass(string seatCode)
+        internal string SeatCode { get; }
+
+        internal int SeatId { get; }
+
+        internal BoardingPass(string seatCode)
         {
+            SeatCode = seatCode;
+            // Conversion to binary to int
             string convertedCode = seatCode.Replace('F', '0').Replace('B', '1').Replace('L', '0').Replace('R', '1');
             SeatId = Convert.ToInt32(convertedCode, 2);
         }
-
-        internal int SeatId { get; }
     }
 }
--- S05/Program.cs
+++ S05/Program.cs
@@ -11,10 +11,15 @@ namespace S05
         {
             Console.WriteLine("Let's get that boarding pass!");
 
-            var boardingPasses = GetBoardingPasses();
-            var highestSeatId = boardingPasses.OrderByDescending(p => p.SeatId).First().SeatId;
+            var boardingPasses = GetBoardingPasses().OrderBy(p => p.SeatId).ToList();
+            boardingPasses.ForEach(p => Console.WriteLine($"Boarding pass {p.SeatCode}, seat ID {p.SeatId}"));
 
+            int highestSeatId = boardingPasses.Last().SeatId;
             Console.WriteLine($"Highest seat ID: {highestSeatId}");
+
+            int lowestId = boardingPasses.First().SeatId;
+            int mySeatId = boardingPasses.Select(p => p.SeatId).Where((id, previousId) => id - previousId == lowestId + 1).First() - 1;
+            Console.WriteLine($"My Seat ID: {mySeatId}");
         }
 
         private static IEnumerable<BoardingPass> GetBoardingPasses()
